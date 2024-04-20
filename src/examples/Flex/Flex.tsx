a03fa93f45651baa51179d20eb5ae897860a7229
forellko <dragonforce97q@gmail.com>
2024-04-20 16:25:17 +0300

    Add blocks with colors

--- src/examples/Flex/Flex.tsx
+++ src/examples/Flex/Flex.tsx
@@ -1,7 +1,13 @@
 import './style.css';
 
 function Flex() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="bg-red-600 w-10 h-10"></div>
+      <div className="bg-green-600 w-10 h-10"></div>
+      <div className="bg-blue-600 w-10 h-10"></div>
+    </div>
+  );
 }
 
 export default Flex;

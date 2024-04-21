b1ba974050c8b0fe7b8126288e87d665db5bf007
forellko <dragonforce97q@gmail.com>
2024-04-21 13:30:08 +0300

    Create ref

--- src/examples/Flex/Flex.tsx
+++ src/examples/Flex/Flex.tsx
@@ -1,17 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './style.css';
 
 function Flex() {
   const [direction, setDirection] = useState('flex-row');
+  const flexRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (!flexRef.current) return;
+  }, []);
 
   return (
     <>
-      <div className={`flex w-[100%] p-10 ${direction}`}>
+      <div
+        ref={flexRef}
+        className={`flex gap-2 w-[100%] p-10 bg-slate-300 ${direction}`}
+      >
         <div className="bg-red-600 w-10 h-10"></div>
         <div className="bg-green-600 w-10 h-10"></div>
         <div className="bg-blue-600 w-10 h-10"></div>
       </div>
       <div className="flex justify-between">
+        <div>
+          <div>
+            <input type="range" name="width" id="range-width" />
+            <input type="range" name="height" id="range-height" />
+          </div>
+        </div>
         <div className="flex flex-col p-10">
           <h2>flex-direction:</h2>
           <button
